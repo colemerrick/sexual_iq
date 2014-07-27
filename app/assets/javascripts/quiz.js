5585348059bf7bfe4a244e66477a490b6b8740a6
Cole Merrick <colegmerrick@gmail.com>
2014-07-27 14:36:04 -0700

    auto fill in score

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -1,23 +1,23 @@
-// hello
-var score = 0;
+// // hello
+// var score = 0;
 
-var questions = [
-	['How many moons does Earth have?', 1],
-	['How many moons does Saturn have?',31],
- 	['How many moons does Venus have?', 0]
-];
+// var questions = [
+// 	['How many moons does Earth have?', 1],
+// 	['How many moons does Saturn have?',31],
+//  	['How many moons does Venus have?', 0]
+// ];
 
-funtion askQuestion(question) {
-	var answer = prompt(question[0],'');
-		if (answer == question[1]) {
-			alert('Correct!');
-			score++;
-		} else {
-			alert('Sorry. The correct answer is ' + question[1]);
-		}
+// funtion askQuestion(question) {
+// 	var answer = prompt(question[0],'');
+// 		if (answer == question[1]) {
+// 			alert('Correct!');
+// 			score++;
+// 		} else {
+// 			alert('Sorry. The correct answer is ' + question[1]);
+// 		}
 
-}
+// }
 
-for (var i=0; i<questions.length; i++) {
-	askQuestion(questions[i]);
-}
+// for (var i=0; i<questions.length; i++) {
+// 	askQuestion(questions[i]);
+// }
